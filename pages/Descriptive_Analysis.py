bd4bd45d25331f622207b26ebe14224a1cbc2444
yrjavk <102833133+yrjavk@users.noreply.github.com>
2024-01-14 20:30:00 +0100

    Update Descriptive_Analysis.py

--- pages/Descriptive_Analysis.py
+++ pages/Descriptive_Analysis.py
@@ -4,23 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-if 'rawdata' not in st.session_state:
-    # Get the data if you haven't
-    rawdata = pd.read_csv("hotel_booking.csv")
-    # Save the data to session state
-    st.session_state.rawdata = rawdata
+
+
 
 rawdata = st.session_state['rawdata']
-descriptive_data = st.session_state['descriptive_data']
-
-cancelled_data = descriptive_data[descriptive_data['is_canceled'] == 1]
-is_canceled_counts = descriptive_data["is_canceled"].value_counts()
-cancelled_percentage = descriptive_data["is_canceled"].value_counts(normalize = True)
-lost_revenues = cancelled_data['total_revenues'].sum()
-non_cancelled_data = descriptive_data[descriptive_data['is_canceled'] == 0]
-top_10_countries_canceled = cancelled_data['country'].value_counts()[:10]
-top_10_countries_canceled_revenues = cancelled_data.groupby('country')['total_revenues'].sum()[:10]
-
 st.markdown("# Descriptive Analysis")
 st.sidebar.markdown("# Descriptive Analysis")
 
@@ -56,23 +43,31 @@ with tab1:
         st.write(rawdata.isna().sum().sort_values(ascending=False))
 
 
+descriptive_data = st.session_state['descriptive_data']
+cancelled_data = descriptive_data[descriptive_data['is_canceled'] == 1]
+is_canceled_counts = descriptive_data["is_canceled"].value_counts()
+cancelled_percentage = descriptive_data["is_canceled"].value_counts(normalize = True)
+lost_revenues = cancelled_data['total_revenues'].sum()
+non_cancelled_data = descriptive_data[descriptive_data['is_canceled'] == 0]
+top_10_countries_canceled = cancelled_data['country'].value_counts()[:10]
+top_10_countries_canceled_revenues = cancelled_data.groupby('country')['total_revenues'].sum()[:10]
 
 
 with tab2:
     if descriptive_data is not None:
 
-        st.subheader('Average daily price')
-        fig = plt.figure(figsize=(6,4))
-        sns.lineplot(x='arrival_date_month', y='adr', hue='hotel', data=descriptive_data)
-        plt.ylabel("Average daily price")
-        plt.xlabel("Months")
-        p = plt.xticks(rotation=30)
+        # Line chart
+        st.subheader('Average Daily Rate (ADR) by Month and Hotel')
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.lineplot(x='arrival_date_month', y='adr', hue='hotel', data=descriptive_data, ax=ax)
+        plt.ylabel("Average Daily Rate (ADR)")
+        plt.xlabel("Month")
+        plt.title("ADR by Month and Hotel")
+        plt.xticks(rotation=45, ha='right')
         st.pyplot(fig)
 
         st.subheader('Cancellation Count')
         st.bar_chart(descriptive_data['is_canceled'].value_counts())
-
-
 
         st.subheader('Top 10 countries with cancellations in %')
         fig = plt.figure(figsize = (6,6))
