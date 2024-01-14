ccff521bfdd85826311eccdcdeaca2dbe5babaf1
yrjavk <102833133+yrjavk@users.noreply.github.com>
2024-01-14 21:05:26 +0100

    Update HotelBookings.py

--- HotelBookings.py
+++ HotelBookings.py
@@ -27,34 +27,39 @@ st.subheader('Raw data')
 @st.cache_data
 def load_data():
     data = pd.read_csv("hotel_booking.csv")
+    data.drop(['email', 'credit_card', 'phone-number', 'name'], axis=1, inplace = True)
     return data
 
 data_load_state = st.text('Loading data...')
 df = load_data()
 data_load_state.text('Loading data...done!')
+
 if st.checkbox("Show raw data first 5 rows"):
-    st.subheader("Raw data")
     st.write(df.head())
 
-st.session_state['df'] = df
+if 'df' not in st.session_state:
+    st.session_state['df'] = df
 
 
 
-
-df.drop(['email', 'credit_card', 'phone-number', 'name'], axis=1, inplace = True)
+#data cleaning for descriptive analysis on streamlit
 df['arrival_date_month'] = pd.to_datetime(df.arrival_date_month, format='%B').dt.month
+df['total_revenues'] = df['adr'] * (df['stays_in_weekend_nights'] + df['stays_in_week_nights'])
 df['reservation_status_date']=df['reservation_status_date'].astype('datetime64[ns]')
-
 df.drop(['agent', 'company'], axis=1, inplace = True)
-
-df.hist(column='children')
-
 df['children'].fillna(df['children'].median(), inplace=True)
 df['children']=df['children'].astype(int)
-
 mode_country=df['country'].mode()[0]
 df['country'] = df['country'].fillna(mode_country)
+df.drop(data['adr'].idxmax(), inplace = True)
+df['total_stay_in_nights'] = df['stays_in_week_nights'] + df['stays_in_weekend_nights']
 
+
+descriptive_data = rawdata_cleaning(rawdata)
+if 'descriptive_data' not in st.session_state:
+    st.session_state['descriptive_data'] = descriptive_data
+
+@st.cache_data
 def factorize_columns(data, columns_to_factorize):
     encoded_mappings = {}
 
@@ -64,7 +69,8 @@ def factorize_columns(data, columns_to_factorize):
         encoded_mappings[column] = {'codes': codes, 'unique_values': unique_values}
 
     return data, encoded_mappings
-
+    
+@st.cache_data
 def replace_undefined_with_mode(data, column_name):
     mode_value = data[column_name].mode()[0]  # Get the mode (most frequent value)
 
@@ -77,44 +83,17 @@ def replace_undefined_with_mode(data, column_name):
 
 
 data, mappings = factorize_columns(df, ['hotel','meal','market_segment','distribution_channel','reserved_room_type','assigned_room_type', 'deposit_type', 'customer_type', 'reservation_status'])
-
-for column, mapping in mappings.items():
-    print("Unique values:", mapping['unique_values'])
-
-
-
-
-data.drop(data['adr'].idxmax(), inplace = True)
-
-
 cancelled_percentage = data["is_canceled"].value_counts(normalize = True)
-data['total_revenues'] = data['adr'] * (data['stays_in_weekend_nights'] + data['stays_in_week_nights'])
-print(cancelled_percentage)
-
-data.head()
-
-
 is_canceled_counts = data["is_canceled"].value_counts()
-
-
-
-
 cancelled_data = data[data['is_canceled'] == 1]
 top_10_countries_canceled = cancelled_data['country'].value_counts()[:10]
-
 lost_revenues = cancelled_data['total_revenues'].sum()
-
 top_10_countries_canceled_revenues = cancelled_data.groupby('country')['total_revenues'].sum()[:10]
-
-
 current_revenues = data[data['is_canceled'] == 0]['total_revenues'].sum()
-
 data[data['is_canceled'] == 0]['is_repeated_guest'].sum()
-
-data['total_stay_in_nights'] = data['stays_in_week_nights'] + data['stays_in_weekend_nights']
-
-
 data.drop(['country','total_revenues','total_stay_in_nights'], axis=1, inplace = True)
+
+
 
 def RMSE(y,y_pred):
   mse=mean_squared_error(y, y_pred)
@@ -132,12 +111,6 @@ st.write(df.columns.tolist())
 
 columns = Xtrain.columns.values.tolist()
 coefs = lr.coef_.ravel().tolist()
-
-
-plt.figure(figsize = (10,6))
-plt.barh(columns, coefs)
-plt.xlabel("Feature importance")
-plt.ylabel("Features")
 
 
 X.drop(["is_repeated_guest"], axis=1, inplace = True)
