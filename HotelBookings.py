7db1a51d0b6eaab5a64ca8e7b7db5deb82aed8b8
yrjavk <102833133+yrjavk@users.noreply.github.com>
2024-01-14 16:26:51 +0100

    Update HotelBookings.py

--- HotelBookings.py
+++ HotelBookings.py
@@ -11,17 +11,35 @@ import xgboost
 import streamlit as st
 
 
-st.write("""
-         #Sales Model""")
+st.title('Hotel Bookings')
+st.markdown(
+    """
+    Welcome to our app to predict the average daily rate of a hotel booking.
+    ### Raw Data Source
+    Check out this [Kaggle Page](https://www.kaggle.com/datasets/mojtaba142/hotel-booking) for the source of the raw data. 
 
-#batch data load functie
-data = pd.read_csv("hotel_booking.csv")
 
-data.head()
+"""
+)
+st.sidebar.markdown("# Hotel Bookings")
+st.subheader('Raw data')
 
-data.info()
+@st.cache_data
+def load_data():
+    data = pd.read_csv("hotel_booking.csv")
+    return data
 
-data.isna().sum().sort_values(ascending=False)
+data_load_state = st.text('Loading data...')
+df = load_data()
+data_load_state.text('Loading data...done!')
+if st.checkbox("Show raw data first 5 rows"):
+    st.subheader("Raw data")
+    st.write(data.head())
+
+st.session_state['df'] = df
+
+
+
 
 data.drop(['email', 'credit_card', 'phone-number', 'name'], axis=1, inplace = True)
 data['arrival_date_month'] = pd.to_datetime(data.arrival_date_month, format='%B').dt.month
@@ -57,7 +75,7 @@ def replace_undefined_with_mode(data, column_name):
     return data
 
 
-data.isna().sum().sort_values(ascending=False)
+
 
 data, mappings = factorize_columns(data, ['hotel','meal','market_segment','distribution_channel','reserved_room_type','assigned_room_type', 'deposit_type', 'customer_type', 'reservation_status'])
 
@@ -66,9 +84,7 @@ for column, mapping in mappings.items():
     print("Unique values:", mapping['unique_values'])
 
 
-fig = plt.figure(figsize =(20, 5))
-plt.boxplot(data['adr'], vert=False)
-plt.show()
+
 
 data.drop(data['adr'].idxmax(), inplace = True)
 
@@ -80,69 +96,27 @@ print(cancelled_percentage)
 data.head()
 
 
-plt.figure(figsize=(5, 4))
-plt.title("Cancellation Count")
 is_canceled_counts = data["is_canceled"].value_counts()
 
-bars = plt.bar(["Not canceled", "Canceled"], is_canceled_counts)
-for bar, count in zip(bars, is_canceled_counts):
-    plt.text(bar.get_x() + bar.get_width() / 2 - 0.1, bar.get_height() + 0.1, str(count), fontsize=10)
 
-
-plt.figure(figsize = (6,2))
-ax1= sns.countplot(x = "hotel", hue = "is_canceled", data = data)
-legend_labels,_ = ax1.get_legend_handles_labels()
-ax1.legend(bbox_to_anchor=(1,1))
-plt.title("Cancellations per hotel", size = 20)
-plt.xlabel("Hotel")
-plt.ylabel("number of reservations")
-plt.legend(["not canceled", "canceled"])
-plt.show
 
 
 cancelled_data = data[data['is_canceled'] == 1]
 top_10_countries_canceled = cancelled_data['country'].value_counts()[:10]
-plt.figure(figsize = (6,6))
-plt.title("Top 10 countries with cancellations in % ")
-plt.pie(top_10_countries_canceled, autopct = "%.2f", labels = top_10_countries_canceled.index)
-plt.show()
 
 lost_revenues = cancelled_data['total_revenues'].sum()
-print(lost_revenues)
 
 
 top_10_countries_canceled_revenues = cancelled_data.groupby('country')['total_revenues'].sum()[:10]
-plt.figure(figsize = (6,6))
-plt.title("Top 10 countries revenue lost due to cancellation in % ")
-plt.pie(top_10_countries_canceled_revenues,autopct = "%.2f",labels = top_10_countries_canceled_revenues.index)
-plt.show()
+
 
 current_revenues = data[data['is_canceled'] == 0]['total_revenues'].sum()
-print(current_revenues)
-
-plt.figure(figsize=(8,4))
-ax1=sns.countplot(x = 'arrival_date_month', data=data, hue='is_canceled')
-legend_labels,_=ax1.get_legend_handles_labels()
-ax1.legend(bbox_to_anchor=[1,1])
-plt.title("Cancellations per month", size=20)
-plt.xlabel('month')
-plt.ylabel('number of reservations')
-plt.legend(['not cancelled','cancelled'])
-plt.show()
 
 
 data[data['is_canceled'] == 0]['is_repeated_guest'].sum()
 
 data['total_stay_in_nights'] = data['stays_in_week_nights'] + data['stays_in_weekend_nights']
 
-data[data['is_canceled'] == 0]
-
-plt.figure(figsize=(6,4))
-sns.lineplot(x='arrival_date_month', y='adr', hue='hotel', data=data)
-plt.ylabel("Average daily price")
-plt.xlabel("Months")
-p = plt.xticks(rotation=30)
-plt.title("Average Daily rate by month")
 
 data.drop(['country','total_revenues','total_stay_in_nights'], axis=1, inplace = True)
 
@@ -165,8 +139,6 @@ lr = LinearRegression()
 lr.fit(Xtrain, ytrain)
 
 
-import matplotlib.pyplot as plt
-
 columns = Xtrain.columns.values.tolist()
 print(columns)
 coefs = lr.coef_.ravel().tolist()
@@ -187,10 +159,6 @@ lr.fit(Xtrain, ytrain)
 columns = Xtrain.columns.values.tolist()
 coefs = lr.coef_.ravel().tolist()
 
-plt.figure(figsize = (10,6))
-plt.barh(columns, coefs)
-plt.xlabel("Feature importance")
-plt.ylabel("Features")
 
 lr.fit(Xtrain, ytrain)
 
@@ -205,12 +173,12 @@ y_pred_lr = lr.predict(Xtest)
 rmse_adr = RMSE(yval, y_pred_lr)
 relative_error = calculate_relative_error(rmse_adr, y)
 
-print("Relative error for the average daily revenue: {:.2f}%".format(relative_error))
+
 
 
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 rmse_scores = cross_val_score(lr, Xtrain, ytrain, cv=kf, scoring='neg_root_mean_squared_error')
-print(" Cross validated RMSE:", -rmse_scores.mean())
+
 
 RMSE(yval, y_pred_lr)
 
@@ -222,12 +190,11 @@ y_pred_xgb = xgb.predict(Xtest)
 rmse_adr = RMSE(yval, y_pred_xgb)
 relative_error = calculate_relative_error(rmse_adr, y)
 
-print("Relative error for the average daily revenue: {:.2f}%".format(relative_error))
+
 
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 rmse_scores = cross_val_score(xgb, Xtrain, ytrain, cv=kf, scoring='neg_root_mean_squared_error')
 
-print("Cross validated RMSE:", -rmse_scores.mean())
 
 RMSE(yval, y_pred_xgb)
 
@@ -238,11 +205,11 @@ y_pred_knn = knn.predict(Xtest)
 rmse_adr = RMSE(yval, y_pred_knn)
 relative_error = calculate_relative_error(rmse_adr, y)
 
-print("Relative error for the average daily revenue: {:.2f}%".format(relative_error))
+
 
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 rmse_scores = cross_val_score(knn, Xtrain, ytrain, cv=kf, scoring='neg_root_mean_squared_error')
 
-print("Cross validated RMSE:", -rmse_scores.mean())
+
 
 RMSE(yval, y_pred_knn)
