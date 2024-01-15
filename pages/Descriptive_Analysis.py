d969ed6deb60b61f4ee77436b550ccde828c0937
yrjavk <102833133+yrjavk@users.noreply.github.com>
2024-01-15 23:11:05 +0100

    Update Descriptive_Analysis.py

--- pages/Descriptive_Analysis.py
+++ pages/Descriptive_Analysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from streamlit_extras.switch_page_button import switch_page
-import plotly.express as px
+
 
 if 'df' not in st.session_state:
     switch_page("Hotel Bookings")
