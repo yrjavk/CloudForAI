9651863baab9c30e85da038bb62129d00c840520
NineYearsOld <45061326+NineYearsOld@users.noreply.github.com>
2024-01-16 23:03:42 +0100

    Update Evaluations.py

--- pages/Evaluations.py
+++ pages/Evaluations.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import joblib
 
 st.markdown("# Evaluations")
 st.sidebar.markdown("# Evaluations")
+
+lr_model = joblib.load('models/lr_model.joblib')
+xgb_model = joblib.load('models/xgb_model.joblib')
+knn_model = joblib.load('models/knn_model.joblib')
+
