d78f34ee5bbe4a4cb361be6b9ee6c808f8ee6b3f
NineYearsOld <45061326+NineYearsOld@users.noreply.github.com>
2024-01-18 21:12:25 +0100

    Update Hotel_Bookings.py

--- Hotel_Bookings.py
+++ Hotel_Bookings.py
@@ -2,18 +2,47 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import xgboost 
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import mean_squared_error
 import streamlit as st
 import joblib
 
-#load data
+
+st.set_page_config(layout="wide")
+
+st.title('Hotel Bookings')
+st.markdown(
+    """
+    Welcome to our app to predict the average daily rate of a hotel booking.  \n
+    In the sidebar you can find different pages to explore the data, make predictions and the evaluations of the model. 
+    
+    ## Raw Data Source
+    Check out this [Kaggle Page](https://www.kaggle.com/datasets/mojtaba142/hotel-booking) for the source of the raw data used in this app. 
+
+
+"""
+)
+st.sidebar.markdown('# Hotel Bookings')
+st.header('Loading data')
+
+
+
 @st.cache_data
 def load_csv_data(filename):
     data = pd.read_csv(filename)
     data.drop(['email', 'credit_card', 'phone-number', 'name'], axis=1, inplace = True)
     return data
+
+
+data_load_state = st.text('Loading raw data...')
+df = load_csv_data('hotel_booking.csv')
+data_load_state.text('Loading raw data...done!')
+
+if st.checkbox("Show raw data first 5 rows"):
+    st.write(df.head())
+
+if 'df' not in st.session_state:
+    st.session_state['df'] = df.copy()
+
+
 
 #data cleaning for descriptive analysis 
 @st.cache_data
@@ -30,46 +59,6 @@ def data_preparation(data):
     data.drop(data['adr'].idxmax(), inplace = True)
     return data
 
-#set categorical columns to numberical values
-@st.cache_data
-def factorize_columns(data, columns_to_factorize):
-    encoded_mappings = {}
-
-    for column in columns_to_factorize:
-        codes, unique_values = pd.factorize(data[column])
-        data[column] = codes
-        encoded_mappings[column] = {'codes': codes, 'unique_values': unique_values}
-
-    return data, encoded_mappings
-
-st.set_page_config(layout="wide")
-
-st.title('Hotel Bookings')
-st.markdown(
-    """
-    Welcome to our app to predict the average daily rate of a hotel booking.  \n
-    In the sidebar you can find different pages to explore the data, make predictions and the evaluations of the model. 
-    
-    ## Raw Data Source
-    Check out this [Kaggle Page](https://www.kaggle.com/datasets/mojtaba142/hotel-booking) for the source of the raw data used in this app. 
-
-
-"""
-)
-
-st.sidebar.markdown('# Hotel Bookings')
-st.header('Loading data')
-
-
-data_load_state = st.text('Loading raw data...')
-df = load_csv_data('hotel_booking.csv')
-data_load_state.text('Loading raw data...done!')
-
-if st.checkbox("Show raw data first 5 rows"):
-    st.write(df.head())
-
-if 'df' not in st.session_state:
-    st.session_state['df'] = df.copy()
 
 data_load_state = st.text('Loading descriptive data...')
 descriptive_data = data_preparation(df.copy())
@@ -80,6 +69,19 @@ if 'descriptive_data' not in st.session_state:
 
 df = data_preparation(df)
 
+
+@st.cache_data
+def factorize_columns(data, columns_to_factorize):
+    encoded_mappings = {}
+
+    for column in columns_to_factorize:
+        codes, unique_values = pd.factorize(data[column])
+        data[column] = codes
+        encoded_mappings[column] = {'codes': codes, 'unique_values': unique_values}
+
+    return data, encoded_mappings
+    
+
 st.header('Modeling Data')
 data_load_state = st.text('Modeling data...')
 
@@ -87,16 +89,15 @@ data_load_state = st.text('Modeling data...')
 df, mappings = factorize_columns(df, ['hotel','meal','market_segment','distribution_channel','reserved_room_type','assigned_room_type', 'deposit_type', 'customer_type', 'reservation_status'])
 st.session_state['mappings'] = mappings
 
-#drop columns used in visualizations only
+#drop columns used in visualizations only 
 df.drop(['country','total_revenues','total_stay_in_nights'], axis=1, inplace = True)
+
 
 y = df[['adr']]
 X = df.drop(['adr', 'reservation_status_date','is_repeated_guest'],axis=1)
 
-Xtrain, Xrest, ytrain, yrest = train_test_split(X, y, test_size=0.2)
-Xval, Xtest, yval, ytest = train_test_split(Xrest, yrest, test_size=0.5)
+Xrest, Xtest, yrest, ytest = train_test_split(x, y, test_size=0.1)
 
-##avoid dataleakage. filling in missing values after train test split
 Xtest['children'].fillna(Xtest['children'].median(), inplace=True)
 ytest['children'].fillna(ytest['children'].median(), inplace=True)
 Xtest['children']=Xtest['children'].astype(int)
@@ -105,7 +106,7 @@ ytest['children']=ytest['children'].astype(int)
 
 lr_model = joblib.load('models/lr_model.joblib')
 if 'lr_model' not in st.session_state:
-    st.session_state['lr_model'] = lr_model
+    st.session_state['lr_model'] = df.copy()
 xgb_model = joblib.load('models/xgb_model.joblib')
 if 'xgb_model' not in st.session_state:
     st.session_state['xgb_model'] = xgb_model
